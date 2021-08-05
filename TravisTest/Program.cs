df69de94933e00326546c47c438169fc63fd99ab
Nikita <nikitabushhan@gmail.com>
2021-08-05 15:46:13 +0300

    create a god

--- TravisTest/Program.cs
+++ TravisTest/Program.cs
@@ -2,12 +2,26 @@
 
 namespace TravisTest
 {
+    public class A
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public A()
+        {
+            name = "Nikita";
+            surname = "Bushchan";
+        }
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("have a good day");
+            var nikita = new A();
+            Console.WriteLine($"Name: {nikita.name}, surname: {nikita.surname}");
         }
     }
 }
